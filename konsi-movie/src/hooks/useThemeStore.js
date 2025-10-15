6ea742931bfffd31013760f4fd767bb9cd97b17c
rjv01 <verma.rajshekhar1@gmail.com>
2025-10-15 18:02:15 +0530

    trying to add daisyui

--- konsi-movie/src/hooks/useThemeStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("movie-theme") || "coffee",
-  setTheme: (theme) => {
-    localStorage.setItem("movie-theme", theme);
-    set({ theme });
-  },
-}));
